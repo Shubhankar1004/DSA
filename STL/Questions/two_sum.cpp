5d5dd385168729058f31b199e0d0928466daf87e
Shubhankar <shubhankarvshastri@gmail.com>
2022-06-01 16:59:38 +0530

    Create two_sum.cpp

---
+++ STL/Questions/two_sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int,int>m; vector<int>v;
+        for (int i=0;i<(int)nums.size();i++)
+        {
+            
+            int diff=target-nums[i];
+            if (m.find(diff) != m.end() && m.find(diff)->second !=i)
+            {
+                v.push_back(m.find(diff)->second);
+                v.push_back(i);
+                return v;
+            }
+            m[nums[i]]=i;
+            
+        }
+        return v;
+    }
