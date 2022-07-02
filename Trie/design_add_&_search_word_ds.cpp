9a54877c12661045de9162294f8ab84b078dab30
Rushil Venkateswar <rushilv14@gmail.com>
2022-07-02 12:22:47 +0530

    Update design_add_&_search_word_ds.cpp

--- Trie/design_add_&_search_word_ds.cpp
+++ Trie/design_add_&_search_word_ds.cpp
@@ -1,58 +1,69 @@
-// https://leetcode.com/problems/design-add-and-search-words-data-structure/
+/*
+    Design add & search words data structure
 
-class Node{
+    Implement trie, handle wildcards: traverse all children & search substrings
+
+    Time: O(m x 26^n) -> m = # of words, n = length of words
+    Space: O(n)
+*/
+
+class TrieNode {
 public:
-    Node *v[26];
-    bool isEnd;
-    Node(): isEnd(false) {
-        for(int i=0; i<26; i++)
-            v[i] = nullptr;
+    TrieNode* children[26];
+    bool isWord;
+    
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isWord = false;
     }
 };
 
 class WordDictionary {
-    Node *root;
-    bool dfs(int j, Node *root, string& word){
-        Node *curr = root;
-        for(int i=j; i<word.size(); i++){
-            char c = word[i];
-            if(c == '.'){
-                // go over the children
-                for(int j=0; j<26; j++){
-                    if(curr->v[j]){
-                        if (dfs(i+1, curr->v[j], word))
-                            return true;
-                    }
-                    else
-                        continue;
-                }
-                return false;
-            }
-            else{
-                if(!curr->v[c-'a'])
-                    return false;
-                curr = curr->v[c-'a'];
-            }
-        }
-        return curr->isEnd;
-    }
 public:
     WordDictionary() {
-        root = new Node();
+        root = new TrieNode();
     }
     
     void addWord(string word) {
-        Node *curr = root;
-        for(char& c: word){
-            if(!curr->v[c-'a'])
-                curr->v[c-'a'] = new Node();
-            curr = curr->v[c-'a'];
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < word.size(); i++) {
+            curr = word[i] - 'a';
+            if (node->children[curr] == NULL) {
+                node->children[curr] = new TrieNode();
+            }
+            node = node->children[curr];
         }
-        curr->isEnd = true;
+        
+        node->isWord = true;
     }
     
     bool search(string word) {
-        return dfs(0, root, word);
+        TrieNode* node = root;
+        return searchInNode(word, 0, node);
+    }
+private:
+    TrieNode* root;
+    
+    bool searchInNode(string& word, int i, TrieNode* node) {
+        if (node == NULL) {
+            return false;
+        }
+        if (i == word.size()) {
+            return node->isWord;
+        }
+        if (word[i] != '.') {
+            return searchInNode(word, i + 1, node->children[word[i] - 'a']);
+        }
+        for (int j = 0; j < 26; j++) {
+            if (searchInNode(word, i + 1, node->children[j])) {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
