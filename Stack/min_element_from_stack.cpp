f279850aefa07c6fe2bfd1e241180a2b44240b62
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-02 15:15:04 +0530

    Update min_element_from_stack.cpp

--- Stack/min_element_from_stack.cpp
+++ Stack/min_element_from_stack.cpp
@@ -97,3 +97,119 @@ int main()
     return 0;
 }
   // } Driver Code Ends
+
+
+
+//O(1) space
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+class Solution{
+    int minEle;
+    stack<int> s;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           
+           //Write your code here
+           if (s.empty())
+           return -1;
+           return (minEle);
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           
+           //Write your code here
+           if (s.empty())
+           return -1;
+           int k=s.top();
+           
+           s.pop();
+           if (k<=0)
+           {
+               
+               int res = minEle;
+               minEle = minEle-k;
+               return res;
+           }
+           return k;
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           
+           //Write your code here
+           if (s.empty())
+           {
+               s.push(x);
+               minEle=x;
+           }
+           else if (x<=minEle)
+           {
+               s.push(x-minEle);
+               minEle=x;
+           }
+           else
+           {
+               s.push(x);
+           }
+       }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        Solution ob;
+        while(q--){
+           int qt;
+           cin>>qt;
+           if(qt==1)
+           {
+              //push
+              int att;
+              cin>>att;
+              ob.push(att);
+           }
+           else if(qt==2)
+           {
+              //pop
+              cout<<ob.pop()<<" ";
+           }
+           else if(qt==3)
+           {
+              //getMin
+              cout<<ob.getMin()<<" ";
+           }
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
