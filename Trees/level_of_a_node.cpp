5a71db262dd995640408f743de3f8332d5f8d21a
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-05 00:59:41 +0530

    Update level_of_a_node.cpp

--- Trees/level_of_a_node.cpp
+++ Trees/level_of_a_node.cpp
@@ -24,3 +24,39 @@ class Solution{
     	return rank;
     }
 };
+
+
+//using level order traversal
+
+
+
+class Solution{
+  public:
+    // function should return level of the target node
+    int getLevel(struct Node *node, int target)
+    {
+    	//code here
+    	queue<Node*>q;
+    	int level=0;int ans=0;
+    	q.push(node);
+    	while (!q.empty())
+    	{
+    	    level++;
+    	    int c=q.size();
+    	    while(c--)
+    	    {
+    	        Node *t=q.front();
+    	        if (t->data==target)
+    	        ans=level;
+    	        if (t->left)
+    	        q.push(t->left);
+    	        if (t->right)
+    	        q.push(t->right);
+    	        q.pop();
+    	    }
+    	    //level++;
+    	}
+    	return ans;
+    }
+};
+
