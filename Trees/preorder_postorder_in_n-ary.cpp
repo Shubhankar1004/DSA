0e069e88e9f80ce4fab3d079bc7a0889c1d2bb81
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-04 17:08:45 +0530

    Create preorder_postorder_in_n-ary.cpp

---
+++ Trees/preorder_postorder_in_n-ary.cpp
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+     void pre(Node *root,vector<int>&v)
+    {
+        if (root==NULL)
+            return;
+         
+         v.push_back(root->val);
+        
+        for (auto child : root->children)
+            pre(child,v);
+        
+        
+    }
+    vector<int> preorder(Node* root) 
+    {
+        vector<int>v;
+        pre(root,v);
+        return v;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    
+    void post(Node *root,vector<int>&v)
+    {
+        if (root==NULL)
+            return;
+        
+        for (auto child : root->children)
+            post(child,v);
+        
+        v.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) 
+    {
+        vector<int>v;
+        post(root,v);
+        return v;
+    }
+};
