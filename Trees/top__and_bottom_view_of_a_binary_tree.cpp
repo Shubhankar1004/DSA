1ea4895418bf525d69deb4f91642538907442ff6
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-06 22:43:55 +0530

    Update and rename top_view_of_a_binary_tree.cpp to top__and_bottom_view_of_a_binary_tree.cpp

--- Trees/top__and_bottom_view_of_a_binary_tree.cpp
+++ Trees/top__and_bottom_view_of_a_binary_tree.cpp
@@ -34,3 +34,38 @@ class Solution
     }
 
 };
+
+
+
+//bottom
+
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root)
+    {
+        // Your Code Here
+        vector<int>ans;
+        map<int,vector<int>>m;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while (!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            Node *t=it.first;
+            int line=it.second;
+            m[line].push_back(t->data);
+            if (t->left)
+            q.push({t->left,line-1});
+            if (t->right)
+            q.push({t->right,line+1});
+        }
+        for (auto i:m)
+        {
+            ans.push_back(i.second[i.second.size()-1]);
+        }
+        return ans;
+    
+    }
+};
