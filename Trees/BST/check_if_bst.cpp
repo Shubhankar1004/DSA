6d72d68b60a6e14a38c1381bc420afaf00ae26dd
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-12 00:40:27 +0530

    Update check_if_bst.cpp

--- Trees/BST/check_if_bst.cpp
+++ Trees/BST/check_if_bst.cpp
@@ -1,1 +1,22 @@
-
+class Solution
+{
+    public:
+    //int flag=1;int maxie=INT_MIN,minie=INT_MAX;
+    //Function to check whether a Binary Tree is BST or not.
+    bool BST(Node* root,int maxie,int minie)
+    {
+        if (!root)
+        return 1;
+        if (root->data>maxie || root->data<minie)
+        return 0;
+        
+        return (BST(root->left,root->data,minie) && BST(root->right,maxie,root->data));
+        
+    }
+    bool isBST(Node* root) 
+    {
+        // Your code here
+        return BST(root,INT_MAX,INT_MIN);
+        //return flag;
+    }
+};
