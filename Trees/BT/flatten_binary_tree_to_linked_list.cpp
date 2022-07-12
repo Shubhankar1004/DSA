034751d952bd8b2522f66a76b5cff30d220abe29
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-12 17:02:33 +0530

    Update flatten_binary_tree_to_linked_list.cpp

--- Trees/BT/flatten_binary_tree_to_linked_list.cpp
+++ Trees/BT/flatten_binary_tree_to_linked_list.cpp
@@ -45,3 +45,31 @@ class Solution
         }
     }
 };
+
+
+//using morris in O(1) space
+
+class Solution
+{
+    public:
+    //Node *curr=NULL;
+    
+    void flatten(Node *root)
+    {
+        //code here
+        Node *curr=root,*prev=NULL;
+        while(curr!=NULL)
+        {
+            if (curr->left!=NULL)
+            {
+                prev=curr->left;
+                while(prev->right)
+                prev=prev->right;
+                prev->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;
+        }
+    }
+};
