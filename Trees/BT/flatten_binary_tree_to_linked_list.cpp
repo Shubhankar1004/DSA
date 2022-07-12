fa4a6f8386ef0ebaa718d9222de00f589c5f5cf8
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-12 16:28:41 +0530

    Update flatten_binary_tree_to_linked_list.cpp

--- Trees/BT/flatten_binary_tree_to_linked_list.cpp
+++ Trees/BT/flatten_binary_tree_to_linked_list.cpp
@@ -15,3 +15,33 @@ class Solution
         prev=root;
     }
 };
+
+
+//using stack
+
+
+class Solution
+{
+    public:
+    Node *prev=NULL;
+    
+    void flatten(Node *root)
+    {
+        //code here
+        stack<Node*>st;
+        st.push(root);
+        while(!st.empty())
+        {
+            Node *curr=st.top();
+            st.pop();
+            if (curr->right)
+            st.push(curr->right);
+            if (curr->left)
+            st.push(curr->left);
+            
+            if (!st.empty())
+            curr->right=st.top();
+            curr->left=NULL;
+        }
+    }
+};
