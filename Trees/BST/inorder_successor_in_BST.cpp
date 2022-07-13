595d17ea40060c096276ab6646dcd127d705820a
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-13 13:09:11 +0530

    Update inorder_successor_in_BST.cpp

--- Trees/BST/inorder_successor_in_BST.cpp
+++ Trees/BST/inorder_successor_in_BST.cpp
@@ -30,3 +30,34 @@ class Solution{
         
     }
 };
+
+
+//optimised soln in time O(height) and space O(1)
+
+
+class Solution{
+  public:
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        //Your code here
+        Node *succ=NULL;
+        while(root!=NULL)
+        {
+            if (root->data<x->data)
+            {
+                root=root->right;
+            }
+            else if (root->data>x->data)
+            {
+                succ=root;
+                root=root->left;
+            }
+            else
+            {
+                root=root->right;
+            }
+        }
+        return succ;
+    }
+};
