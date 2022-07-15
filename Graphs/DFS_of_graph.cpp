c3ad06a8cfbe3173c6dc3833eab0f684f1306c66
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-16 01:58:39 +0530

    Create DFS_of_graph.cpp

---
+++ Graphs/DFS_of_graph.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfshelp(int node,vector<int>& vis,vector<int> adj[],vector<int> &dfs)
+    {
+        dfs.push_back(node);
+        vis[node]=1;
+        for (auto it:adj[node])
+        {
+            if (!vis[it])
+            {
+                //dfs.push_back(it);
+                //vis[it]=1;
+                dfshelp(it,vis,adj,dfs);
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) 
+    {
+        // Code here
+        vector<int>dfs;
+        vector<int>vis(V,0);
+        for (int i=0;i<V;i++)
+        {
+            if (!vis[i])
+            {
+                dfshelp(i,vis,adj,dfs);
+            }
+        }
+        return dfs;
+    }
+};
