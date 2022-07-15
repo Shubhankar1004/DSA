99916e4e8d58f95c7c55de6384b290a13e9587b2
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-16 00:13:08 +0530

    Create BFS_of_a_graph.cpp

---
+++ Graphs/BFS_of_a_graph.cpp
@@ -0,0 +1,77 @@
+//directed graph
+
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) 
+    {
+        // Code here
+        vector<int> bfs;
+        vector<int> vis(V,0);
+        
+        
+                queue<int>q;
+                q.push(0);
+                vis[0]=1;
+                while(!q.empty())
+                {
+                    int node=q.front();
+                    q.pop();
+                    bfs.push_back(node);
+                    
+                    for (auto it:adj[node])
+                    {
+                        if (!vis[it])
+                        {
+                            q.push(it);
+                            vis[it]=1;
+                        }
+                    }
+                }
+        return bfs;
+    }
+};
+
+
+
+//multiple components in the graph
+
+
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) 
+    {
+        // Code here
+        vector<int> bfs;
+        vector<int> vis(V,0);
+        
+        for (int i=0;i<V;i++)
+        {
+            if (!vis[i])
+            {
+                queue<int>q;
+                q.push(i);
+                vis[i]=1;
+                while(!q.empty())
+                {
+                    int node=q.front();
+                    q.pop();
+                    bfs.push_back(node);
+                    
+                    for (auto it:adj[node])
+                    {
+                        if (!vis[it])
+                        {
+                            q.push(it);
+                            vis[it]=1;
+                        }
+                    }
+                }
+            }
+        }
+        return bfs;
+    }
+};
