53c10932ae826d22a1c481f229e84d534c4dfd88
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-16 20:41:42 +0530

    Update topological_sort.cpp

--- Graphs/topological_sort.cpp
+++ Graphs/topological_sort.cpp
@@ -1,3 +1,6 @@
+//using dfs
+
+
 class Solution
 {
 	public:
@@ -35,5 +38,51 @@ class Solution
 	        st.pop();
 	    }
 	    return ans;
+	}
+};
+
+
+
+//using bfs : Kahns algorithm
+
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int>in(V,0);
+	    vector<int>ans;
+	    queue<int>q;
+	    for (int i=0;i<V;i++)
+	    {
+	        for (auto it:adj[i])
+	        {
+	            in[it]++;
+	        }
+	    }
+	    for (int i=0;i<V;i++)
+	    {
+	        if (in[i]==0)
+	        q.push(i);
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int node =q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for (auto it:adj[node])
+	        {
+	            in[it]--;
+	            if (in[it]==0)
+	            q.push(it);
+	        }
+	        
+	    }
+	    return ans;
+	    
 	}
 };
