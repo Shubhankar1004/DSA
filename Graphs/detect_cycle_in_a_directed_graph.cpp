2e17a94246fb47c58fba249f9e2244e3f8feef2e
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-16 21:44:07 +0530

    Update detect_cycle_in_a_directed_graph.cpp

--- Graphs/detect_cycle_in_a_directed_graph.cpp
+++ Graphs/detect_cycle_in_a_directed_graph.cpp
@@ -42,3 +42,53 @@
         return 0;
     }
 };
+
+
+
+//using Kahns algorithm
+
+
+
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) 
+    {
+        // code here
+        vector<int>in(V,0);
+	    vector<int>ans;
+	    queue<int>q;
+	    for (int i=0;i<V;i++)
+	    {
+	        for (auto it:adj[i])
+	        {
+	            in[it]++;
+	        }
+	    }
+	    for (int i=0;i<V;i++)
+	    {
+	        if (in[i]==0)
+	        q.push(i);
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int node =q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for (auto it:adj[node])
+	        {
+	            in[it]--;
+	            if (in[it]==0)
+	            q.push(it);
+	        }
+	        
+	    }
+	    for (int i=0;i<V;i++)
+	    {
+	        if (in[i]!=0)
+	        return 1;
+	    }
+	    return 0;
+    }
+};
