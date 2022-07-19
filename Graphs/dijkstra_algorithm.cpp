f0e44ffa96f060ecd00a92e3bcd0e56c5d55a72e
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-19 15:38:32 +0530

    Create dijkstra_algorithm.cpp

---
+++ Graphs/dijkstra_algorithm.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        vector<int>dis(V,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        dis[S]=0;
+        pq.push({0,S});
+        //vector<int>ans;
+        while(!pq.empty())
+        {
+            int prev=pq.top().second;
+            int prevdis=pq.top().first;
+            pq.pop();
+            for (auto it:adj[prev])
+            {
+                int next=it[0];
+                int nextdis=it[1];
+                if (dis[prev]+nextdis<dis[next])
+                {
+                    dis[next]=dis[prev]+nextdis;
+                    pq.push({dis[next],next});
+                }
+            }
+        }
+        return dis;
+    }
+};
