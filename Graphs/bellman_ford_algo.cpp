6bdf7a726cc04225681b5a423dc0cad08e5fd67a
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-22 19:20:22 +0530

    Update bellman_ford_algo.cpp

--- Graphs/bellman_ford_algo.cpp
+++ Graphs/bellman_ford_algo.cpp
@@ -29,3 +29,47 @@ class Solution{
         return dist;
     }
 };
+
+
+
+
+//detecting a negative weight cycle using bellman ford
+
+
+
+class Solution {
+public:
+	
+	int isNegativeWeightCycle(int n, vector<vector<int>>edges)
+    {
+        // Code here
+        vector<int>dist(n,1e8);
+        dist[0]=0;
+        for (int i=0;i<n-1;i++)
+        {
+            vector<int>temp;
+            int u,v,wt;
+            for (int j=0;j<edges.size();j++)
+            {
+                temp=edges[j];
+                u=temp[0];
+                v=temp[1];
+                wt=temp[2];
+                if (dist[u]+wt<dist[v])
+                dist[v]=dist[u]+wt;
+            }
+        }
+        vector<int>temp;
+        int u,v,wt;
+        for (int j=0;j<edges.size();j++)
+        {
+            temp=edges[j];
+            u=temp[0];
+            v=temp[1];
+            wt=temp[2];
+            if (dist[u]+wt<dist[v])
+            return 1;
+        }
+        return 0;;
+    }
+};
