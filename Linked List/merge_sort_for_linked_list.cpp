324ef6929143c6d7dc67b6eb9f2a14d3e3ecdbb5
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-28 15:33:14 +0530

    Create merge_sort_for_linked_list.cpp

---
+++ Linked List/merge_sort_for_linked_list.cpp
@@ -0,0 +1,105 @@
+class Solution{
+ 
+ 
+public:
+Node* findMid(Node* head) {
+   Node* slow = head;
+   Node* fast = head -> next;
+   
+   while(fast != NULL && fast -> next != NULL) {
+       slow = slow -> next;
+       fast = fast -> next -> next; 
+   }
+   return slow;
+}
+
+Node* merge(Node* left, Node* right) {
+   
+   if(left == NULL)
+       return right;
+   
+   if(right == NULL)
+       return left;
+   
+   Node* ans = new Node(-1);
+   Node* temp = ans;
+   
+   //merge 2 sorted Linked List
+   while(left != NULL && right != NULL) {
+       if(left -> data < right -> data ) {
+           temp -> next = left;
+           temp = left;
+           left = left -> next;
+       }
+       else
+       {
+           temp -> next = right;
+           temp = right;
+           right = right -> next;
+       }
+   }
+   
+   while(left != NULL) {
+       temp -> next = left;
+       temp = left;
+       left = left -> next;
+   }
+   
+   while(right != NULL) {
+       temp -> next = right;
+       temp = right;
+       right = right -> next;
+   }
+   
+   ans = ans -> next;
+   return ans;
+   
+}
+
+public:
+
+Node* mergeSort(Node *head) {
+
+   //base case
+
+   if( head == NULL || head -> next == NULL ) {
+
+       return head;
+
+   }
+
+   
+
+  
+
+   Node* mid = findMid(head);
+
+   
+
+   Node* left = head;
+
+   Node* right = mid->next;
+
+   mid -> next = NULL;
+
+   
+
+   //recursive calls to sort both halves
+
+   left = mergeSort(left);
+
+   right = mergeSort(right);
+
+   
+
+   //merge both left and right halves
+
+   Node* result = merge(left, right);
+
+   
+
+   return result;
+
+}
+
+};
