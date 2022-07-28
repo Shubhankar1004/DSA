cf00467b97b846a839ab58d42752352320807cca
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-29 00:04:43 +0530

    Create kth_largest_element.cpp

---
+++ Heaps/kth_largest_element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        priority_queue<int,vector<int>,greater<int>>minh;
+        for(int i=0;i<nums.size();i++)
+        {
+            minh.push(nums[i]);
+            if (minh.size()>k)
+            minh.pop();
+        }
+        if (minh.size()>=k)
+        return (minh.top());
+        else
+            return -1;
+    }
+};
