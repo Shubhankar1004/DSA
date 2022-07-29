2d99a123376dc52c1f38733837f78fd3afaf601e
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2022-07-29 15:02:54 +0530

    Create k_largest_elements.cpp

---
+++ Heaps/k_largest_elements.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) 
+	{
+	    // code here
+	    priority_queue<int,vector<int>,greater<int>>minh;
+	    for (int i=0;i<n;i++)
+	    {
+	        minh.push(arr[i]);
+	        if (minh.size()>k)
+	        minh.pop();
+	    }
+	    vector<int>v;
+	    while(!minh.empty())
+	    {
+	        v.push_back(minh.top());
+	        minh.pop();
+	    }
+	    reverse(v.begin(),v.end());
+	    return v;
+	}
+
+};
