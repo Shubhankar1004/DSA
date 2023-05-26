cd33ed91a09cc6cdf773662dde937c81cad0ebbc
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-05-26 20:15:06 +0530

    Create unique_paths.cpp

---
+++ Dynamic Programming/unique_paths.cpp
@@ -0,0 +1,25 @@
+//recursion only has a time complexity of O(2^(m*n))
+
+class Solution {
+public:
+    int help(int i,int j,vector<vector<int>> &dp)
+    {
+        if (i==0 and j==0)
+        return 1;
+        
+        if (i<0 || j<0) return 0;
+        if (dp[i][j]!=-1)
+        return dp[i][j];
+        int left=help(i-1,j,dp);
+        int right=help(i,j-1,dp);
+        int sum=left + right;
+        return dp[i][j]=sum;
+    }
+    int uniquePaths(int m, int n) 
+    {
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        int ans=help(m-1,n-1,dp);
+        return ans;
+    }
+};
