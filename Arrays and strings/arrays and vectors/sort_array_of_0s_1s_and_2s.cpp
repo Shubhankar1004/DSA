4f6aa58ee759dbc435908f426d80cd7ddaa126a8
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-05-27 23:25:01 +0530

    Create sort_array_of_0s_1s_and_2s.cpp

---
+++ Arrays and strings/arrays and vectors/sort_array_of_0s_1s_and_2s.cpp
@@ -0,0 +1,22 @@
+//Dutch National Flag Algorithm
+//Intuition written in copy
+//works in O(n) time with O(1) space
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
