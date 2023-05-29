198ba9bc8caf939224a428e56d9b16226335e52c
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-05-29 22:40:54 +0530

    Update is_there_a_subsequence_with_sum_k.cpp

--- Dynamic Programming/is_there_a_subsequence_with_sum_k.cpp
+++ Dynamic Programming/is_there_a_subsequence_with_sum_k.cpp
@@ -25,3 +25,44 @@ bool subsetSumToK(int n, int k, vector<int> &arr)
     vector<vector<int>> dp(n,vector<int> (k+1,-1));
     return help(arr.size()-1,k,arr,dp);
 }
+
+
+//Tabulation
+
+bool tab(int n,int tar,vector<int>&a){
+
+    
+
+    vector<vector<bool>>dp(n,vector<bool>(tar+1,0));
+
+ 
+
+    for(int i=0;i<n;i++)
+
+    dp[i][0] = 1;
+
+    dp[0][a[0]] = 1;
+
+    for(int i =1;i<n;i++){
+
+        for(int j=1;j<=tar;j++){
+
+            bool excl = dp[i-1][j];
+
+            bool inclu = 0;
+
+            if(j-a[i]>=0)
+
+            inclu = dp[i-1][j-a[i]];
+
+            dp[i][j] = (inclu | excl);
+
+        }
+
+    }
+
+    if(dp[n-1][tar]) return 1;
+
+    return 0;
+
+}
