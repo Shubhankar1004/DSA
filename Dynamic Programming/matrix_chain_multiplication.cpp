f1b59576ff996127cc99ed502f16f206a48afab9
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-06-13 19:50:26 +0530

    Update matrix_chain_multiplication.cpp

--- Dynamic Programming/matrix_chain_multiplication.cpp
+++ Dynamic Programming/matrix_chain_multiplication.cpp
@@ -19,3 +19,33 @@ public:
         return help(1,n-1,arr,dp);
     }
 };
+
+
+//Tabulation
+
+
+class Solution{
+public:
+    int matrixMultiplication(int n, int arr[])
+    {
+        // code here
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for (int i=0;i<n;i++)
+        dp[i][i]=0;
+        
+        for (int i=n-1;i>0;i--)
+        {
+            for (int j=i+1;j<n;j++)
+            {
+                int mini=1e9;//necessary to initialize it here only
+                for (int k=i;k<j;k++)
+                {
+                    int op=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    mini=min(mini,op);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][n-1];
+    }
+};
