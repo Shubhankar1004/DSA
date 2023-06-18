3626a221905e21d538098218c5f0e006d4cb174a
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-06-18 19:12:58 +0530

    Update subset_sums(print_all_possible_sums_with_given_array.cpp

--- Recursion/subset_sums(print_all_possible_sums_with_given_array.cpp
+++ Recursion/subset_sums(print_all_possible_sums_with_given_array.cpp
@@ -1,10 +1,5 @@
 //print sum of all subsets in array
 
-// { Driver Code Starts
-#include<bits/stdc++.h> 
-using namespace std; 
-
- // } Driver Code Ends
 class Solution
 {
 public:
@@ -29,26 +24,3 @@ public:
     }
 };
 
-// { Driver Code Starts.
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int N;
-        cin>>N;
-        vector<int> arr(N);
-        for(int i = 0 ; i < N ; i++){
-            cin >> arr[i];
-        }
-        Solution ob;
-        vector<int> ans = ob.subsetSums(arr,N);
-        sort(ans.begin(),ans.end());
-        for(auto sum : ans){
-            cout<< sum<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}  // } Driver Code Ends
