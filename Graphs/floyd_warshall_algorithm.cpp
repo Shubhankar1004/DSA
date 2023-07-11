c2dcad8c39e8c4fa07c013e3f5232e42a8cfb968
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-07-11 22:02:30 +0530

    Create floyd_warshall_algorithm.cpp

---
+++ Graphs/floyd_warshall_algorithm.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix)
+	{
+	    // Code here
+	    int n=matrix.size();
+	    int m=matrix[0].size();
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<m;j++)
+	        {
+	            if(matrix[i][j]==-1)
+	            {
+	                matrix[i][j]=1e9;
+	            }
+	            if (i==j) matrix[i][j]=0;
+	        }
+	    }
+	    
+	    for(int via=0;via<m;via++)
+	    {
+	         for(int i=0;i<n;i++)
+	         {
+	            for(int j=0;j<m;j++)
+	            {
+	                matrix[i][j]=min(matrix[i][j],matrix[i][via]+matrix[via][j]);
+	            }
+	         }
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(matrix[i][j]==1e9){
+	                matrix[i][j]=-1;
+	            }
+	        }
+	    }
+	    
+	}
+};
