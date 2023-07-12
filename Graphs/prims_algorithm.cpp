1a4400dfed03ac4ca93a08bcc5b5057cb74fecfc
Shubhankar <80578052+Shubhankar1004@users.noreply.github.com>
2023-07-12 21:32:23 +0530

    Update prims_algorithm.cpp

--- Graphs/prims_algorithm.cpp
+++ Graphs/prims_algorithm.cpp
@@ -1,69 +1,31 @@
-//Prim's and Kruskal's Algorithm are the famous greedy algorithms. They are used for finding the Minimum Spanning Tree (MST) of a given graph. 
-//To apply these algorithms, the given graph must be weighted, connected and undirected.
-
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-	int N,m;
-	cin >> N >> m;
-	vector<pair<int,int> > adj[N]; 
-
-	int a,b,wt;
-	for(int i = 0; i<m ; i++){
-		cin >> a >> b >> wt;
-		adj[a].push_back(make_pair(b,wt));
-		adj[b].push_back(make_pair(a,wt));
-	}	
-	
-	int parent[N]; 
-      
-    int key[N]; 
-      
-    bool mstSet[N]; 
-  
-    for (int i = 0; i < N; i++) 
-        key[i] = INT_MAX, mstSet[i] = false; 
-    
-    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
-
-    key[0] = 0; 
-    parent[0] = -1; 
-    pq.push({0, 0});
-    // Run the loop till all the nodes have been visited
-    // because in the brute code we checked for mstSet[node] == false while computing the minimum
-    // but here we simply take the minimal from the priority queue, so a lot of times a node might be taken twice
-    // hence its better to keep running till all the nodes have been taken. 
-    // try the following case: 
-    // Credits: Srejan Bera
-    // 6 7 
-    // 0 1 5 
-    // 0 2 10 
-    // 0 3 100 
-    // 1 3 50 
-    // 1 4 200
-    // 3 4 250
-    // 4 5 50 
-    while(!pq.empty())
-    { 
-        int u = pq.top().second; 
-        pq.pop(); 
-        
-        mstSet[u] = true; 
-        
-        for (auto it : adj[u]) {
-            int v = it.first;
-            int weight = it.second;
-            if (mstSet[v] == false && weight < key[v]) {
-                parent[v] = u;
-		key[v] = weight; 
-                pq.push({key[v], v});    
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});//wt,node
+        vector<int> vis(V,0);
+        //vis[0]=1;
+        int sum=0;
+        while(!pq.empty())
+        {
+            auto node=pq.top().second;
+            auto wt=pq.top().first;
+            pq.pop();
+            if(vis[node]==1)continue;
+            vis[node]=1;
+            sum+=wt;
+            for (auto it:adj[node])
+            {
+                if (!vis[it[0]])
+                {
+                    pq.push({it[1],it[0]});
+                }
             }
         }
-            
-    } 
-    
-    for (int i = 1; i < N; i++) 
-        cout << parent[i] << " - " << i <<" \n"; 
-	return 0;
-}
+        return sum;
+    }
+};
